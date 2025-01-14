def35a749b32f86f3bea711f4318e18fa8383831
NATALIA MARIANO FASCINI <natalia.mfascini@senacsp.edu.br>
2025-01-14 15:21:04 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá mundo! Sou Natalia");
+Console.WriteLine("Estou aprendendo Lógica de Programação.");
---
+++ ex03/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sou Natalia Mariano Fascini.");
+Console.WriteLine("Estou estudando no Senac do Largo Treze.");
